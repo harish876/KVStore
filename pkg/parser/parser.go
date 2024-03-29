10f3118e1aab8027075d1d623a53fe3995e6ec94
harish876 <harishgokul01@gmail.com>
2024-03-29 16:39:46 +0530

    get and set , improved parser a little bit

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -6,65 +6,72 @@ import (
 	"strings"
 )
 
-type RespMessage struct {
-	Method        string
-	Message       string
-	MessageLength int
-	MethodLength  int
-	SegmentLength int //Number of segments - rename it later
+type RESPMessage struct {
+	Method         string
+	Messages       []string
+	MessagesLength int
+	SegmentLength  int //Number of segments - rename it later
 }
 
-// TODO: Improve parser
-func Parse(msg []byte) RespMessage {
+// TODO: Improve parser logic
+func Decode(msg []byte) (RESPMessage, error) {
 	strMsg := string(msg)
 	splitMsg := strings.Split(strMsg, "\r\n")
+
 	var parsedArray []string
 	var parsedLenArray []int
-	var parsedRespMessage RespMessage
+	var parsedRespMessage RESPMessage
+
 	for _, v := range splitMsg {
+		if strings.Contains(v, "*") {
+			segLenArray := strings.Split(v, "*")
+			if len(segLenArray) != 2 {
+				return parsedRespMessage, fmt.Errorf("error parsing segment. segment length not provided")
+			}
+			length, err := strconv.Atoi(segLenArray[1])
+			if err != nil {
+				return parsedRespMessage, fmt.Errorf("error parsing segment length: %v", err)
+			}
+			parsedRespMessage.SegmentLength = length
+			continue
+		}
+
 		if strings.Contains(v, "$") {
 			msgLenArray := strings.Split(v, "$")
 			if len(msgLenArray) == 2 {
 				length, err := strconv.Atoi(msgLenArray[1])
 				if err != nil {
-					fmt.Printf("Error parsing message length: %v", err)
+					return parsedRespMessage, fmt.Errorf("error parsing message length: %v", err)
 				}
 				parsedLenArray = append(parsedLenArray, length)
 			}
 			continue
 		}
-		if strings.Contains(v, "*") {
-			segLenArray := strings.Split(v, "*")
-			if len(segLenArray) == 2 {
-				length, err := strconv.Atoi(segLenArray[1])
-				if err != nil {
-					fmt.Printf("Error parsing message length: %v", err)
-				}
-				parsedRespMessage.SegmentLength = length
-			}
-			continue
-		}
+
 		if v == "" {
+			//END OF MESSAGE
 			continue
 		}
 		parsedArray = append(parsedArray, v)
 	}
-	if len(parsedArray) == 1 {
-		//This is a ping command, which doesnt contain any method
-		parsedRespMessage.Method = "ping"
-		parsedRespMessage.MethodLength = 4
-		parsedRespMessage.Message = parsedArray[0]
+
+	if len(parsedLenArray) == 0 {
+		return parsedRespMessage, nil
+	} else if len(parsedArray) == 1 {
+		parsedRespMessage.Method = strings.ToLower(parsedArray[0])
 	} else if len(parsedArray) >= 2 {
 		parsedRespMessage.Method = strings.ToLower(parsedArray[0])
-		parsedRespMessage.Message = parsedArray[1]
+		parsedRespMessage.Messages = parsedArray[1:]
 	}
 
-	if len(parsedLenArray) == 1 {
-		//There was no method in the request
-		parsedRespMessage.MessageLength = parsedLenArray[0]
-	} else if len(parsedArray) >= 2 {
-		parsedRespMessage.MethodLength = parsedLenArray[0]
-		parsedRespMessage.MessageLength = parsedLenArray[1]
+	if parsedRespMessage.SegmentLength > 0 && parsedRespMessage.SegmentLength-1 != len(parsedRespMessage.Messages) {
+		return parsedRespMessage, fmt.Errorf("unable to parse message. incorrect segment length or number of messages")
 	}
-	return parsedRespMessage
+	parsedRespMessage.MessagesLength = len(parsedRespMessage.Messages)
+	return parsedRespMessage, nil
+}
+
+// TODO: make a encode function to make life easier
+func Encode(msg RESPMessage) ([]byte, error) {
+	return nil, nil
 }
--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -9,37 +9,49 @@ import (
 
 func TestParserEcho(t *testing.T) {
 	input := []byte("*2\r\n$4\r\necho\r\n$3\r\nhey\r\n")
-	msg := parser.Parse(input)
-	fmt.Printf("Method: %s\nMessage: %s\nMessage length: %d\nMethod Length: %d\nSegment Length: %d\n", msg.Method, msg.Message, msg.MessageLength, msg.MethodLength, msg.SegmentLength)
+	msg, _ := parser.Decode(input)
+	fmt.Printf("\nMethod: %s\nMessage: %s\nMessage length: %d\nSegment Length: %d\n", msg.Method, msg.Messages, msg.MessagesLength, msg.SegmentLength)
 
 	if msg.Method != "echo" {
 		t.Fatalf("Test Failed. Expected Method: echo\nRecieved Method: %s\n", msg.Method)
 	}
-	if msg.Message != "hey" {
-		t.Fatalf("Test Failed. Expected Message: hey\nRecieved Method: %s\n", msg.Message)
+	if msg.MessagesLength > 1 && msg.Messages[0] != "hey" {
+		t.Fatalf("Test Failed. Expected Messages: hey\nRecieved Method: %s\n", msg.Messages)
 	}
-	if msg.MessageLength != 3 {
-		t.Fatalf("Test Failed. Expected Messagelength: 3\nRecieved Method: %d\n", msg.MessageLength)
-	}
-	if msg.MethodLength != 4 {
-		t.Fatalf("Test Failed. Expected MethodLength: 4\nRecieved Method: %d\n", msg.MethodLength)
+	if msg.MessagesLength > 1 && len(msg.Messages[0]) != 3 {
+		t.Fatalf("Test Failed. Expected Messagelength: 3\nRecieved Method: %d\n", msg.MessagesLength)
 	}
 }
 func TestParserPing(t *testing.T) {
 	input := []byte("*1\r\n$4\r\nping\r\n")
-	msg := parser.Parse(input)
-	fmt.Printf("Method: %s\nMessage: %s\nMessage length: %d\nMethod Length: %d\nSegment Length: %d\n", msg.Method, msg.Message, msg.MessageLength, msg.MethodLength, msg.SegmentLength)
+	msg, _ := parser.Decode(input)
+	fmt.Printf("\nMethod: %s\nMessage: %s\nMessage length: %d\nSegment Length: %d\n", msg.Method, msg.Messages, msg.MessagesLength, msg.SegmentLength)
 
 	if msg.Method != "ping" {
 		t.Fatalf("Test Failed. Expected Method: ping\nRecieved Method: %s\n", msg.Method)
 	}
-	if msg.Message != "ping" {
-		t.Fatalf("Test Failed. Expected Message: \nRecieved Method: %s\n", msg.Message)
+	if len(msg.Messages) != 0 {
+		t.Fatalf("Test Failed. Expected Messages: \nRecieved Method: %s\n", msg.Messages)
 	}
-	if msg.MessageLength != 4 {
-		t.Fatalf("Test Failed. Expected Messagelength: 0\nRecieved Method: %d\n", msg.MessageLength)
+	if msg.MessagesLength != 0 {
+		t.Fatalf("Test Failed. Expected Messagelength: 0\nRecieved Method: %d\n", msg.MessagesLength)
 	}
-	if msg.MethodLength != 4 {
-		t.Fatalf("Test Failed. Expected MethodLength: 4\nRecieved Method: %d\n", msg.MethodLength)
+}
+
+func TestParserSet(t *testing.T) {
+	input := []byte("*3\r\n$3\r\nset\r\n$4\r\nfoo\r\nbar\r\n")
+	msg, err := parser.Decode(input)
+	if err != nil {
+		t.Fatal(err)
 	}
+	fmt.Printf("\nMethod: %s\nMessage: %s\nMessage length: %d\nSegment Length: %d\n", msg.Method, msg.Messages, msg.MessagesLength, msg.SegmentLength)
+}
+
+func TestParserGet(t *testing.T) {
+	input := []byte("*2\r\n$3\r\nget\r\n$3\r\nbar\r\n")
+	msg, err := parser.Decode(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("\nMethod: %s\nMessage: %s\nMessage length: %d\nSegment Length: %d\n", msg.Method, msg.Messages, msg.MessagesLength, msg.SegmentLength)
 }
